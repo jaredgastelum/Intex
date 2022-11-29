f010a65d240ac01e7895ba935fee73add0ce577f
Joshua Allen <joshutallen99@gmail.com>
2022-11-29 16:48:14 -0700

    API changes

--- IntexProject/settings.py
+++ IntexProject/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'kidney',
         'USER': 'postgres',
-        'PASSWORD': '1210',
+        'PASSWORD': getpass(),
         'HOST': 'localhost'
     }
 }
