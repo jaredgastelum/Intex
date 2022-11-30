5d7b237f6187b7f8d3c97c9a311c4c6e8f730a6c
joshminer3 <josh.miner356@gmail.com>
2022-11-29 17:23:06 -0700

    ligma3

--- KidneyApp/urls.py
+++ KidneyApp/urls.py
@@ -5,12 +5,14 @@ from .views import dashboardPageView
 from .views import profilePageView
 from .views import storeProfilePageView
 from .views import storeVitalsPageView
+from .views import labVitalsPageView
 
 urlpatterns = [
     path('', indexPageView, name='index'),
     path('about', aboutPageView, name='about'),
     path('dashboard', dashboardPageView, name='dashboard'),
     path('profile', profilePageView, name='profile'),
+    path('labvitals', labVitalsPageView, name="labvitals"),
     path("storeprofile", storeProfilePageView, name="storeProfile"),
     path('storevitals', storeVitalsPageView, name='storevitals'),
 ]
