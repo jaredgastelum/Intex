7387b7556b5fe47c4bcb3077c135551f32898345
Jared Gastelum <jaredgastelum@gmail.com>
2022-11-30 20:51:15 -0700

    wassuppp

--- KidneyApp/views.py
+++ KidneyApp/views.py
@@ -250,11 +250,33 @@ def signup(request):
         pass1 = request.POST['pass1']
         pass2 = request.POST['pass2']
 
+
+        if User.objects.filter(username=username):
+            messages.error(request, "Username already exist! Please enter another username")
+            return redirect('newLogin')
+        
+        if User.objects.filter(email=email):
+            messages.error(request, "Email already registered!")
+            return redirect('newLogin')
+
+        if len(username)>10:
+            messages.error(request, "Username must be under 10 characters")
+        
+        if pass1 != pass2 :
+            messages.error(request, "Passwords didn't match!")
+
+        if not username.isalnum():
+            messages.error(request, "Username must include at least 1 letter")
+            return redirect('newLogin')
+
         myuser = User.objects.create_user(username, email, pass1)
         myuser.first_name = fname
         myuser.last_name = lname
 
         myuser.save()
+
+
+
 
         messages.success(request, "Your Account has been successfully created.")
 
